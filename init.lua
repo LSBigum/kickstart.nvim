e0604dce705f9479ea16fbdfd4d20006ad4ef35c
Laus Bigum <laus@nordbo-robotics.com>
2025-04-23 14:07:03 +0200

    Set colors for linenumbers

--- init.lua
+++ init.lua
@@ -105,5 +105,10 @@ require 'lazy-bootstrap'
 -- [[ Configure and install plugins ]]
 require 'lazy-plugins'
 
+-- Set relative line number as orange (do this after loading theme)
+vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = 'grey' })
+vim.api.nvim_set_hl(0, 'LineNr', { fg = 'orange' })
+vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = 'grey' })
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
