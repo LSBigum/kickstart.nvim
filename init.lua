18e543e160d44f606bbe234cbbea4d050047f8fd
Laus Bigum <laus@nordbo-robotics.com>
2025-05-24 00:04:58 +0800

    Add tmux rename when changing windows

--- init.lua
+++ init.lua
@@ -109,6 +109,8 @@ require 'lazy-plugins'
 
 require 'lsp.clangd'
 
+require 'tmux-configuration'
+
 -- Set relative line number as orange (do this after loading theme)
 vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = 'grey' })
 vim.api.nvim_set_hl(0, 'LineNr', { fg = 'orange' })
